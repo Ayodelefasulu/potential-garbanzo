6b13bdaa60ac28bb1321dbca12008b1a58d4628e
ayodelefasulu <fasuluayodele@gmail.com>
2024-11-30 03:26:28 +0100

    don't really know what

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
 
+    // ignore: unused_local_variable
     Widget page;
     switch (selectedIndex) {
       case 0:
